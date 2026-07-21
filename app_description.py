5f3f841aeb2b413b6796e612bbd26afbf167de79
Tanvi Jaiswal <tanvijaiswal034@gmail.com>
2026-07-21 23:36:31 +0530

    Added Pizza Corner's app description

---
+++ app_description.py
@@ -0,0 +1,15 @@
+"""
+Creating a simple pizza parlour app where one can browse and order items :
+
+- Categories in the app :  Menu , View Cart and Quit
+- One can add, delete or update an item from the cart
+- One can also view the cart and the total bill
+- The review section will be available to the customer once the order is complete
+
+Navigation Flow:
+Main Menu → Restaurant Menu → Food Categories
+Food Category → Add Items to Cart
+View Cart → Edit / Remove Items → Payment
+Payment → Feedback → Main Menu 
+
+"""
